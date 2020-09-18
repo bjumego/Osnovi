bffe35f50eefea85861f0b5170390896e6714c32
Sergei <sergei.smirnov001@ivkhk.ee>
2020-09-18 10:05:45 +0300

    Начало работы

---
+++ src/osnoviprogramirovanija/OsnoviProgramirovanija.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package osnoviprogramirovanija;
+
+/**
+ *
+ * @author user
+ */
+public class OsnoviProgramirovanija {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        Task1.run();
+    }
+    
+}
---
+++ src/osnoviprogramirovanija/Task1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package osnoviprogramirovanija;
+
+/**
+ *
+ * @author user
+ */
+class Task1 {
+
+    public void run() {
+        System.out.println("Введите число градусов по Цельсию: ");
+        // rewenie zadachi
+        System.out.println("По Фарингейту это будет: ");
+        
+    }
+    
+}
