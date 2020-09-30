e71bc49ff59305b1226aeb1b94aa1e7d753b7f0a
Sergei <sergei.smirnov001@ivkhk.ee>
2020-09-30 09:19:27 +0300

    4 zadachi sdelano

--- src/startdew/StartDew.java
+++ src/startdew/StartDew.java
@@ -18,8 +18,17 @@ public class StartDew {
         //Task1 task1 = new Task1();
           //task1.run();
         
-        Task2 task2 = new Task2();
-        task2.run();
+        //Task2 task2 = new Task2();
+        //task2.run();
+        
+        //Task3 task3 = new Task3();
+        //task3.run();
+        
+        //Task4 task4 = new Task4();
+        //task4.run();
+        
+        Task5 task5 = new Task5();
+        task5.run();
     }
     
 }
---
+++ src/startdew/Task5.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package startdew;
+
+/**
+ *
+ * @author user
+ */
+class Task5 {
+       public void run(){
+        System.out.println("----- Задача 5 ------");
+        System.out.println("Создаем ДВУХМЕРНЫЙ массив  из 10х5 ячеек типа int, \nзаполняем его случайными числами и выводим \nего значения в цикле");
+       int[][] a=new int[10][5];
+        for (int[] a1 : a) {
+            for 
+            
+            
+            
+            
+        System.out.println("----- конец задачи 5 ------");
+    }
+}
+    
+}
